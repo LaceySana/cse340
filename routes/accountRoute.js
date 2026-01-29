38ee9f5864c4ad01d7fad1969227f66f2dfb94e3
LaceySana <laceysanabria@gmail.com>
2026-01-28 17:32:25 -0700

    login view created

---
+++ routes/accountRoute.js
@@ -0,0 +1,9 @@
+// Needed Resources
+const express = require("express")
+const router = new express.Router()
+const utilities = require('../utilities');
+const acctController = require("../controllers/accountController")
+
+router.get("/login", acctController.buildLogin)
+
+module.exports = router;
--- server.js
+++ server.js
@@ -12,7 +12,33 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const accountRoute = require("./routes/accountRoute")
 const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require('./database/')
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
 
 /* ***********************
  * View Engine and Templates
@@ -29,6 +55,8 @@ app.use(static)
 app.get("/", utilities.handleErrors(baseController.buildHome))
 // Inventory routes
 app.use("/inv", utilities.handleErrors(inventoryRoute))
+// Account routes
+app.use("/account", utilities.handleErrors(accountRoute))
 // Error Route
 app.use("/err", utilities.handleErrors(baseController))
 // File Not Found Route - must be last route in list
