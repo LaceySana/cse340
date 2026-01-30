144c8528e9274069ebecff475026627c983ae040
LaceySana <laceysanabria@gmail.com>
2026-01-30 12:30:12 -0700

    input validation added

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,12 +1,18 @@
 // Needed Resources
 const express = require("express")
 const router = new express.Router()
-const utilities = require('../utilities')
+const utilities = require("../utilities")
 const acctController = require("../controllers/accountController")
+const regValidate = require("../utilities/account-validation");
 
 router.get("/login", acctController.buildLogin)
 router.get("/register", acctController.buildRegister)
 // Route for register form action
-router.post('/register', utilities.handleErrors(acctController.registerAccount))
+router.post(
+    "/register", 
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(acctController.registerAccount)
+)
 
 module.exports = router;
